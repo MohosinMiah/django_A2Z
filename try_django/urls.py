678b2de595be80651625f22d1eac1727730bfd36
mohosin16103308 <hamza1610330816@gmail.com>
2022-11-28 23:44:25 +0600

    Articals App Created

--- try_django/urls.py
+++ try_django/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+# from .views import home_view 
+from try_django import views 
 
 urlpatterns = [
+    # path( '' , home_view ),
+    path( '' , views.home_view ),
     path('admin/', admin.site.urls),
 ]
--- try_django/views.py
+++ try_django/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
 
 
-def index(request):
+def home_view(request):
     return HttpResponse("Hello, world. You're at the polls index.")
