c69399d26eb9cf49737af09da22fb1299a57b3a5
mohosin16103308 <hamza1610330816@gmail.com>
2022-11-30 00:42:44 +0600

    Added Artical CRUD django admin and display data based on id

--- try_django/views.py
+++ try_django/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
 
+from articals.models import Article
 
 def home_view(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    name = "Bangladesh"
+    # Get data from database
+    artical_obj = Article.objects.get( pk=1 )
+
+    title   = artical_obj.title
+    content = artical_obj.content
+    # Django Template
+    CONTENT = f""" Artical Title : {name}  , and Artical Content : {content} """
+    return HttpResponse( CONTENT )
