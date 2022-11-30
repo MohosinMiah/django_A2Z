3d64f893c9c82fa6f5b0e3e5927482490780bb4e
mohosin16103308 <hamza1610330816@gmail.com>
2022-11-30 22:21:19 +0600

    List Data display and rendered

--- try_django/views.py
+++ try_django/views.py
@@ -7,6 +7,10 @@ from django.shortcuts import render
 def home_view(request):
     # Get data from database
     artical_obj = Article.objects.get( pk=1 )
+    articals = Article.objects.all()
 
-    context = {'artical_obj': artical_obj}
+    context = {
+        'artical_obj': artical_obj,
+        'articals' : articals
+        }
     return render(request, 'home_view.html',context)
