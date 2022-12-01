919075a9d2c5c6df23a00e2ceb49cf3454870d4c
mohosin16103308 <hamza1610330816@gmail.com>
2022-12-01 23:51:34 +0600

    Artical Form Data Saved

--- articals/urls.py
+++ articals/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_view, name='artical'),
+    path('', views.home_view, name='artical_index'),
+    path('artical/create', views.artical_create, name='artical_create'),
+    path('artical/create/post', views.artical_create_post, name='artical_create_post'),
     path('<int:artical_id>/', views.artical_detail, name='artical_detail'),
 
 ]
--- articals/views.py
+++ articals/views.py
@@ -13,7 +13,7 @@ def home_view(request):
         'artical_obj': artical_obj,
         'articals' : articals
         }
-    return render(request, 'home_view.html',context)
+    return render(request, 'articals/home_view.html',context)
 
 
 def artical_detail(request, artical_id):
@@ -23,4 +23,19 @@ def artical_detail(request, artical_id):
     context = {
         'artical_obj': artical_obj,
         }
-    return render(request, 'artical_details_view.html',context)
+    return render(request, 'articals/artical_details_view.html',context)
+
+
+def artical_create(request):
+    
+    context = {}
+    return render(request, 'articals/create.html')
+
+
+def artical_create_post(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        print(title)
+        content = request.POST.get('content')
+        Article.objects.create(title=title, content=content)
+    return render(request, 'articals/create.html')
