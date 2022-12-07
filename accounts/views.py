28ef6b20bc310034b7cc965dd107c692a0270e69
mohosin16103308 <hamza1610330816@gmail.com>
2022-12-07 23:09:10 +0600

    Django Default Login

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
 
 # Create your views here.
 def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page.
+            return redirect('/admin/')
+            ...
+        else:
+            return render( request, 'accounts/login.html', {} )
+
     return render( request, 'accounts/login.html', {} )
 
 
